eb8d394657a2220825249f7003dd30102222807e
Philippa Bather <bather1984@hotmail.com>
2022-08-15 16:17:52 +0200

    View: basic layout of GUI

--- src/com/philippa/javaFXPigGame/Controller/Controller.java
+++ src/com/philippa/javaFXPigGame/Controller/Controller.java
@@ -1,4 +1,12 @@
 package com.philippa.javaFXPigGame.Controller;
 
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+
 public class Controller {
+
+    @FXML
+    public void handleExit() {
+        Platform.exit();
+    }
 }
