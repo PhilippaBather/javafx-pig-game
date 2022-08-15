229c87ce8d91b523e623b7005605df83c8b41330
Philippa Bather <bather1984@hotmail.com>
2022-08-15 12:51:45 +0200

    Main: scene size revised; fxml name refractored

--- src/com/philippa/javaFXPigGame/Main.java
+++ src/com/philippa/javaFXPigGame/Main.java
@@ -10,9 +10,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("View/sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("View/mainwindow.fxml"));
+        primaryStage.setTitle("Pig Game");
+        primaryStage.setScene(new Scene(root, 900, 500));
         primaryStage.show();
     }
 
